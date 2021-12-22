c1e2a5bf1ea24675f88dfcf81866ce432223dd4b
giorgiozannini <44499913+giorgiozannini@users.noreply.github.com>
2021-12-22 10:41:59 +0100

    back to target

--- yolox/data/datasets/coco_classes.py
+++ yolox/data/datasets/coco_classes.py
@@ -3,16 +3,5 @@
 # Copyright (c) Megvii, Inc. and its affiliates.
 
 COCO_CLASSES = (
-  "ignored_region",
-  "pedestrian",
-  "people",
-  "bycicle",
-  "car",
-  "van",
-  "truck",
-  "tricycle",
-  "awning-tricycle",
-  "bus",
-  "motor",
-  "others"
+  "target",
 )
