ead2230853efc6f0eecb3114f88183b69a2497bf
giorgiozannini <44499913+giorgiozannini@users.noreply.github.com>
2021-12-22 15:25:01 +0100

    num_classes = 1

--- exps/default/yolox_tiny.py
+++ exps/default/yolox_tiny.py
@@ -15,5 +15,6 @@ class Exp(MyExp):
         self.scale = (0.5, 1.5)
         self.random_size = (10, 20)
         self.test_size = (416, 416)
+        self.num_classes = 1
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         self.enable_mixup = False
