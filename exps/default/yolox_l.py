c2e3faec58c5cecb96091db90b1d59825528ca7a
giorgiozannini <44499913+giorgiozannini@users.noreply.github.com>
2022-02-09 09:44:51 +0100

    changed output dir to valohai outputs and size

--- exps/default/yolox_l.py
+++ exps/default/yolox_l.py
@@ -13,11 +13,11 @@ class Exp(MyExp):
         self.depth = 1.0
         self.width = 1.0
         self.scale = (0.5, 1.5)
-        self.random_size = (70, 90)
-        self.test_size = (2560, 2560)
-        self.input_size = (2560, 2560)
+        self.random_size = (30, 50)
+        self.test_size = (1280, 1280)
+        self.input_size = (1280, 1280)
         self.num_classes = 1
         abs_path = os.path.realpath(__file__)
-        self.output_dir = abs_path[:abs_path.index("YOLO")]
+        self.output_dir = "/valohai/outputs"
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
